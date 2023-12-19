6262d21f97423fae0fd77044f096645c901e9efe
Paul mazeau <mazeau.paul@gmail.com>
2023-12-19 19:44:31 +0100

    divide the video in X 1 minutes video

--- TIkTok_Edit.py
+++ TIkTok_Edit.py
@@ -1,5 +1,6 @@
 import moviepy.editor as mpy
 from moviepy.video.fx.all import crop, resize
+import random
 
 # Fonction pour cropper et redimensionner un clip
 def process_clip(filename):
@@ -23,8 +24,36 @@ def process_clip(filename):
 resized_clip1 = process_clip("video1.mp4")
 resized_clip2 = process_clip("video2.mp4")
 
+# Durée minimale des deux clips
+min_duration = min(resized_clip1.duration, resized_clip2.duration)
+
+# Couper les clips à la durée minimale
+resized_clip1 = resized_clip1.subclip(0, min_duration)
+resized_clip2 = resized_clip2.subclip(0, min_duration)
+
 # Combinaison des clips en une colonne
 combined_clip = mpy.clips_array([[resized_clip1], [resized_clip2]])
 
-# Écriture du clip final
-combined_clip.write_videofile('combined_video.mp4')
+# Découpage et exportation des segments avec durée aléatoire
+current_time = 0
+part = 1
+while current_time < min_duration:
+    # Durée aléatoire du segment entre 61 et 90 secondes
+    segment_duration = random.randint(61, 90)
+
+    # Calculer le temps de fin du segment
+    end_time = current_time + segment_duration
+
+    # S'assurer que le temps de fin ne dépasse pas la durée totale de la vidéo
+    if end_time > combined_clip.duration:
+        end_time = combined_clip.duration
+
+    # Découper le segment
+    segment = combined_clip.subclip(current_time, end_time)
+
+    # Exporter le segment
+    segment.write_videofile(f'combined_video_part_{part}.mp4')
+
+    # Mettre à jour le temps de début pour le prochain segment et incrémenter le compteur de partie
+    current_time = end_time
+    part += 1
